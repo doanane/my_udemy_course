d19d5cbcf227e5bb72d6d29260b3dd2692ee4274
Desond Anane <doanane@st.ug.edu.gh>
2024-03-10 21:41:39 +0000

    adding new staffs

---
+++ news-updater.py
@@ -0,0 +1,15 @@
+#Your API key is: 760d32d30f664d54921e94f8e295651e
+from newsapi import NewsApiClient
+
+# Init
+newsapi = NewsApiClient(api_key='760d32d30f664d54921e94f8e295651e')
+
+# /v2/top-headlines
+top_headlines = newsapi.get_top_headlines(q='bitcoin',
+                                          category='business',
+                                          language='en')
+print(top_headlines)
+# dt=top_headlines['articles']
+# for x,y in enumerate(dt):
+#     print(f'{x} {y["description"]}')
+
